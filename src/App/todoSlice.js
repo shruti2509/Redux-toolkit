5c5d7e4a9e84d6b5521ce96656f9ebdcbaa8a25e
aparna0908 <95964393+aparna0908@users.noreply.github.com>
2023-09-20 00:40:28 +0530

    edited

--- src/App/todoSlice.js
+++ src/App/todoSlice.js
@@ -1,27 +1,57 @@
 import {createSlice, nanoid} from "@reduxjs/toolkit"
 
-const initialState ={
-    todos: [{id: 1, text: 'Hello'}],
-}
+// const initialState ={
+//     todos: [{id: 1, text: 'Hello'}],
+// }
 
 export const todoSlice = createSlice({
     name: 'todo',
-    initialState,
+    initialState: [],
     reducers: {
         addTodo: (state, action)=>{
-          const todo ={
-            id: nanoid(),
-            text: action.payload,
-          }
-         state.todos.push(todo)
+        //   const todo ={
+        //     id: nanoid(),
+        //     text: action.payload,
+        //   }
+         state.push(action.payload)
         },
         removeTodo: (state, action) => {
-            state.todos = state.todos.filter((todo) =>
-            todo.id !== action.payload)
+            // state.todos = state.todos.filter((todo) =>
+            // todo.id !== action.payload)
+            state.splice(action.payload, 1)
+            console.log(action.payload)
         },
         updateUser: (state, action) => {
             const { id, text } = action.payload;
+            // const updatedTodos = state.todos.map(todo => {
+            //     // Check if the todo item has the specified id
+            //     if (todo.id === id) {
+            //         // If it does, create a new object with the updated text
+            //         return { ...todo, text: newText };
+            //     }
+            //     // If it doesn't, leave the todo item unchanged
+            //     return todo;
+            // });
+        
+            // // Return a new state object with the updated todos
+            // return { ...state, todos: updatedTodos };
+
+
+
+            
             state[id] = text;
+
+             // Check if the todo item has the specified id
+        // if (todo.id === id) {
+        //     // If it does, create a new object with the updated text
+        //     return { ...todo, text: newText };
+        // }
+        // If it doesn't, leave the todo item unchanged
+        // return todo;
+    // });
+
+    // Return a new state object with the updated todos
+    // return { ...state, todos: updatedTodos };
         },
     }
 })
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -8,10 +8,10 @@ import { removeTodo, updateUser} from '../App/todoSlice'
 
 function Todos() {
     
-    const [editableUser, setEditableUser] = useState({id: null, text: '' });
+    const [editableUser, setEditableUser] = useState({id: null, text: ""});
     const editUser = (id) => {
       if (editableUser.id === null) {
-        setEditableUser({ id, text: todos[id] });
+        setEditableUser({ id});
       } else {
         
         dispatch(updateUser({ id: editableUser.id, text: editableUser.text }));
@@ -24,30 +24,30 @@ function Todos() {
     };
   
 
-    const todos = useSelector(state => state.todos)
+    const todos = useSelector(state => state.users)
     console.log(todos);
     const dispatch = useDispatch()
   return (
    <>
    <strong><h4>Todo List</h4></strong>
    {todos.map((todo, id) => (
-     <li className='mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded' key ={todo.id} style={{color: 'white'}}>
+     <li className='mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded' key ={id} style={{color: 'white'}}>
       
-      {editableUser.id === todo.id ? (
+      {editableUser.id === id ? (
             <input style={{color: 'black'}}
               type="text"
               value={editableUser.text}
               onChange={handleInputChange}
             />
           ):(
-      <span>{todo.text}</span> 
+      <span>{todo}</span> 
       )}
     <button className='text-white bg-red-500 border-0 py-1 px-4 focus: outline-none hover: bg-red-600 rounded text-md' 
     
-    onClick={() => dispatch(removeTodo(todo.id))} >delete</button>
+    onClick={() => dispatch(removeTodo(id))} >delete</button>
     
-          <button onClick={() => editUser(todo.id)}>
-          {editableUser.id === todo.id ?  <div>save</div> : <div>edit</div> }
+          <button onClick={() => editUser(id)}>
+          {editableUser.id === id ?  <div>save</div> : <div>edit</div> }
           </button>
     </li>
     
